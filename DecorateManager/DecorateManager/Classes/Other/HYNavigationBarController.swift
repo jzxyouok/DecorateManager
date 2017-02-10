256b63dfd4e48bef2d7a96dcc38a052fbcea0b38
hansuo <hccong_go@163.com>
2017-02-10 10:37:42 +0800

    删除pod 更改架构

---
+++ DecorateManager/DecorateManager/Classes/Other/HYNavigationBarController.swift
@@ -0,0 +1,15 @@
+//
+//  HYNavigationBarController.swift
+//  DecorateManager
+//
+//  Created by Harry on 2017/2/9.
+//  Copyright © 2017年 Harry. All rights reserved.
+//
+
+import UIKit
+
+class HYNavigationBarController: UINavigationController {
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ DecorateManager/DecorateManager/Classes/Other/HYTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  HYTabBarController.swift
+//  DecorateManager
+//
+//  Created by Harry on 2017/2/9.
+//  Copyright © 2017年 Harry. All rights reserved.
+//
+
+import UIKit
+
+class HYTabBarController : UITabBarController {
+    override func viewDidLoad() {
+        
+    }
+}
